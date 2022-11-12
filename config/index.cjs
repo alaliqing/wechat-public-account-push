409ad69098d39e724ccb6c5995a38200ce7f255f
Jing_Qin <53362513+alaliqing@users.noreply.github.com>
2022-11-12 15:59:25 +0100

    template update

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
       id: 'ofXps5sgcfzqk98BvPSTL1BsKa9g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'KcceFZPrF9DBzdQq1Uo9hPVDHq3jxt6SUGjwR92YrZ8',
+      useTemplateId: '2TQLC3SnTKrvw6vq49fz6Vz-cFj1bqOyY8OyD0CXKPM',
       // 所在省份或城市，也可以不填
       province: '',
       // 所在城市或县区
