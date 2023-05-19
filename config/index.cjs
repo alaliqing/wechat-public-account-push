71a1cd7c54c70247be22842b47fb5bb2f65c51c2
Jing_Qin <53362513+alaliqing@users.noreply.github.com>
2023-05-19 17:54:48 +0200

    Update index.cjs
    special day update

--- config/index.cjs
+++ config/index.cjs
@@ -114,7 +114,7 @@ const USER_CONFIG = {
       id: 'ofXps5sgcfzqk98BvPSTL1BsKa9g',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: '2TQLC3SnTKrvw6vq49fz6Vz-cFj1bqOyY8OyD0CXKPM',
+      useTemplateId: 'mjXcWB6Pj2Tt5FjW_ti1PMOzUcvVotW_nPv2xwu2DLw',
       // 所在省份或城市，也可以不填
       province: '',
       // 所在城市或县区
